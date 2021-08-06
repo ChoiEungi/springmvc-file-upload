278453ef2707dedf5f085fa1154436b4ea684ef9
ChoiEungi <choieungi@gm.gist.ac.kr>
2021-08-06 15:16:17 +0900

    init

---
+++ src/main/java/com/example/springmvcfileupload/SpringmvcFileUploadApplication.java
@@ -0,0 +1,13 @@
+package com.example.springmvcfileupload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmvcFileUploadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringmvcFileUploadApplication.class, args);
+    }
+
+}
