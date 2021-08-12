42a4140f71edd64af23cc75e85d8e667d6e548c1
ChoiEungi <choieungi@gm.gist.ac.kr>
2021-08-12 11:23:25 +0900

    file upload

--- src/main/java/com/example/springmvcfileupload/controller/MainController.java
+++ src/main/java/com/example/springmvcfileupload/controller/MainController.java
@@ -1,2 +1,30 @@
-package com.example.springmvcfileupload.controller;public class MainController {
+package com.example.springmvcfileupload.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Slf4j
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+
+    @GetMapping("/file-upload")
+    public String assignment(){
+        return "assignment";
+    }
+
+    @PostMapping("/file-upload")
+    public String assignmentSubmit(){
+        return "submission";
+    }
+
 }
+
